3fb542d0b0c317700b08557074bb76186a199474
Morten Bojer <morten.bojer@gmail.com>
2017-11-16 08:58:59 +0000

    Files merges not sure its working

--- lib/wBus/wBus.cpp
+++ lib/wBus/wBus.cpp
@@ -2,11 +2,23 @@
 Version 0.2
 */
 
+// --------------------------------------------- From MBWire ---------------------------------------------
+
+extern "C" {
+  #include <stdlib.h>
+  #include <string.h>
+  #include <inttypes.h>
+  #include "twi.h"
+}
+
+
+
+// --------------------------------------------- wBus ---------------------------------------------
+
 #include "Arduino.h"
 #include "wBus.h"
 
-#include <MBWire.h>
-
+// --------------------------------------------- wBus ---------------------------------------------
 
 wBus::wBus(int I2C_Device_ID, int Max_Queue_Length, bool Log_To_Serial, long Serial_Speed) {
 
@@ -19,6 +31,249 @@ wBus::wBus(int I2C_Device_ID, int Max_Queue_Length, bool Log_To_Serial, long Ser
 
 } // End Marker for wBus
 
+// Initialize Class Variables //////////////////////////////////////////////////
+
+uint8_t wBus::rxBuffer[BUFFER_LENGTH];
+uint8_t wBus::rxBufferIndex = 0;
+uint8_t wBus::rxBufferLength = 0;
+
+uint8_t wBus::txAddress = 0;
+uint8_t wBus::txBuffer[BUFFER_LENGTH];
+uint8_t wBus::txBufferIndex = 0;
+uint8_t wBus::txBufferLength = 0;
+
+uint8_t wBus::transmitting = 0;
+void (*wBus::user_onRequest)(void);
+void (*wBus::user_onReceive)(int);
+
+
+// --------------------------------------------- From MBWire ---------------------------------------------
+
+// Public Methods //////////////////////////////////////////////////////////////
+
+void wBus::begin(void)
+{
+  rxBufferIndex = 0;
+  rxBufferLength = 0;
+
+  txBufferIndex = 0;
+  txBufferLength = 0;
+
+  twi_init();
+}
+
+void wBus::begin(uint8_t address)
+{
+  twi_setAddress(address);
+  twi_attachSlaveTxEvent(onRequestService);
+  twi_attachSlaveRxEvent(onReceiveService);
+  begin();
+}
+
+void wBus::begin(int address)
+{
+  begin((uint8_t)address);
+}
+
+uint8_t wBus::requestFrom(uint8_t address, uint8_t quantity)
+{
+  // clamp to buffer length
+  if(quantity > BUFFER_LENGTH){
+    quantity = BUFFER_LENGTH;
+  }
+  // perform blocking read into buffer
+  uint8_t read = twi_readFrom(address, rxBuffer, quantity);
+  // set rx buffer iterator vars
+  rxBufferIndex = 0;
+  rxBufferLength = read;
+
+  return read;
+}
+
+uint8_t wBus::requestFrom(int address, int quantity)
+{
+  return requestFrom((uint8_t)address, (uint8_t)quantity);
+}
+
+void wBus::beginTransmission(uint8_t address)
+{
+  // indicate that we are transmitting
+  transmitting = 1;
+  // set address of targeted slave
+  txAddress = address;
+  // reset tx buffer iterator vars
+  txBufferIndex = 0;
+  txBufferLength = 0;
+}
+
+void wBus::beginTransmission(int address)
+{
+  beginTransmission((uint8_t)address);
+}
+
+uint8_t wBus::endTransmission(void)
+{
+  // transmit buffer (blocking)
+  int8_t ret = twi_writeTo(txAddress, txBuffer, txBufferLength, 1);
+  // reset tx buffer iterator vars
+  txBufferIndex = 0;
+  txBufferLength = 0;
+  // indicate that we are done transmitting
+  transmitting = 0;
+  return ret;
+}
+
+// must be called in:
+// slave tx event callback
+// or after beginTransmission(address)
+size_t wBus::write(uint8_t data)
+{
+  if(transmitting){
+  // in master transmitter mode
+    // don't bother if buffer is full
+    if(txBufferLength >= BUFFER_LENGTH){
+      setWriteError();
+      return 0;
+    }
+    // put byte in tx buffer
+    txBuffer[txBufferIndex] = data;
+    ++txBufferIndex;
+    // update amount in buffer
+    txBufferLength = txBufferIndex;
+  }else{
+  // in slave send mode
+    // reply to master
+    twi_transmit(&data, 1);
+  }
+  return 1;
+}
+
+// must be called in:
+// slave tx event callback
+// or after beginTransmission(address)
+size_t wBus::write(const uint8_t *data, size_t quantity)
+{
+  if(transmitting){
+  // in master transmitter mode
+    for(size_t i = 0; i < quantity; ++i){
+      write(data[i]);
+    }
+  }else{
+  // in slave send mode
+    // reply to master
+    twi_transmit(data, quantity);
+  }
+  return quantity;
+}
+
+// must be called in:
+// slave rx event callback
+// or after requestFrom(address, numBytes)
+int wBus::available(void)
+{
+  return rxBufferLength - rxBufferIndex;
+}
+
+// must be called in:
+// slave rx event callback
+// or after requestFrom(address, numBytes)
+int wBus::read(void)
+{
+  int value = -1;
+
+  // get each successive byte on each call
+  if(rxBufferIndex < rxBufferLength){
+    value = rxBuffer[rxBufferIndex];
+    ++rxBufferIndex;
+  }
+
+  return value;
+}
+
+// must be called in:
+// slave rx event callback
+// or after requestFrom(address, numBytes)
+int wBus::peek(void)
+{
+  int value = -1;
+
+  if(rxBufferIndex < rxBufferLength){
+    value = rxBuffer[rxBufferIndex];
+  }
+
+  return value;
+}
+
+void wBus::flush(void)
+{
+  // XXX: to be implemented.
+}
+
+// behind the scenes function that is called when data is received
+void wBus::onReceiveService(uint8_t* inBytes, int numBytes)
+{
+  // don't bother if user hasn't registered a callback
+  if(!user_onReceive){
+    return;
+  }
+  // don't bother if rx buffer is in use by a master requestFrom() op
+  // i know this drops data, but it allows for slight stupidity
+  // meaning, they may not have read all the master requestFrom() data yet
+  if(rxBufferIndex < rxBufferLength){
+    return;
+  }
+  // copy twi rx buffer into local read buffer
+  // this enables new reads to happen in parallel
+  for(uint8_t i = 0; i < numBytes; ++i){
+    rxBuffer[i] = inBytes[i];
+  }
+  // set rx iterator vars
+  rxBufferIndex = 0;
+  rxBufferLength = numBytes;
+  // alert user program
+  user_onReceive(numBytes);
+}
+
+// behind the scenes function that is called when data is requested
+void wBus::onRequestService(void)
+{
+  // don't bother if user hasn't registered a callback
+  if(!user_onRequest){
+    return;
+  }
+  // reset tx buffer iterator vars
+  // !!! this will kill any pending pre-master sendTo() activity
+  txBufferIndex = 0;
+  txBufferLength = 0;
+  // alert user program
+  user_onRequest();
+}
+
+// sets function called on slave write
+void wBus::onReceive( void (*function)(int) )
+{
+  user_onReceive = function;
+}
+
+// sets function called on slave read
+void wBus::onRequest( void (*function)(void) )
+{
+  user_onRequest = function;
+}
+
+void wBus::pullup(bool Activate) {
+  if (Activate == true) {
+    digitalWrite(SDA, 1);
+    digitalWrite(SCL, 1);
+  }
+  else {
+    digitalWrite(SDA, 0);
+    digitalWrite(SCL, 0);
+  }
+}
+
+
+// --------------------------------------------- wBus ---------------------------------------------
 
 
 void wBus::Boot_Message() {
@@ -77,11 +332,11 @@ int wBus::Device_ID_Check() {
 	String test_string = "1337"; // CHANGE ME
 	String I2C_BUS_Responce;
 
-	Wire.begin(100);
+	begin(100);
 
-	Wire.beginTransmission(100);  // broadcast to all
-	Wire.write(test_string.c_str());
-	I2C_BUS_Responce = Wire.endTransmission();
+	beginTransmission(100);  // broadcast to all
+	write(test_string.c_str());
+	I2C_BUS_Responce = endTransmission();
 
 	if (I2C_BUS_Responce != "") { // CHANGE ME
 		Serial.println(I2C_BUS_Responce);
@@ -136,9 +391,9 @@ void wBus::Broadcast(String Broadcast_String) {
 
 	int I2C_BUS_Responce;
 
-	Wire.beginTransmission (0);  // broadcast to all
-	Wire.write(Broadcast_String.c_str());
-	I2C_BUS_Responce = Wire.endTransmission();
+	beginTransmission (0);  // broadcast to all
+	write(Broadcast_String.c_str());
+	I2C_BUS_Responce = endTransmission();
 
 	if (I2C_BUS_Responce == 0) { // REMOVE ME
 		Serial.println(String("Send: ") + String(Broadcast_String)); // REMOVE ME
--- lib/wBus/wBus.h
+++ lib/wBus/wBus.h
@@ -5,66 +5,110 @@ Network for made to run on the I2C bus using broadcast
 */
 
 #ifndef wBus_h
-#define wBus_h
+  #define wBus_h
 
-#include "Arduino.h"
+  // From MWWire (WSWire)
 
+  #include <inttypes.h>
+  #include "Stream.h"
+  #define BUFFER_LENGTH 32
 
-class wBus {
-
-public:
-
-  wBus(int I2C_Device_ID, int Max_Queue_Length, bool Log_To_Serial, long Serial_Speed);
-
-  void Boot_Message();
-
-  int Device_ID_Check();
-
-  void I2C_BUS_Error(int Error_Number);  
-
-  void Broadcast(String Broadcast_String);
-
-  // --------------------------------------------- I2C Command Queue ---------------------------------------------
-
-  void Queue_Push(String Push_String, bool Add_To_Front_Of_Queue);
-  String Queue_Pop();
-  String Queue_Peek();
-  String Queue_Peek_Queue();
-  int Queue_Length();
-  int Queue_Is_Empthy();
-  void Queue_Clear();
-  String Queue_Search_Peek(String Search_String);
-  String Queue_Search_Pop(String Search_String, bool Delete_All_Matches);
+  // End Marker MBWire (WSWire)
 
 
-  // --------------------------------------------- Misc ---------------------------------------------
+  #include "Arduino.h"
 
-  void Error_Blink(int Number_Of_Blinks);
+  class wBus : public Stream {
+
+    public:
+      // --------------------------------------------- From MBWire ---------------------------------------------
+
+      void begin();
+      void begin(uint8_t);
+      void begin(int);
+      void beginTransmission(uint8_t);
+      void beginTransmission(int);
+      uint8_t endTransmission(void);
+      uint8_t requestFrom(uint8_t, uint8_t);
+      uint8_t requestFrom(int, int);
+      virtual size_t write(uint8_t);
+      virtual size_t write(const uint8_t *, size_t);
+      virtual int available(void);
+      virtual int read(void);
+      virtual int peek(void);
+  	  virtual void flush(void);
+      void onReceive( void (*)(int) );
+      void onRequest( void (*)(void) );
+      void pullup(bool Activate);
+
+       using Print::write; // Change ME / REMOVE ME
+
+      // --------------------------------------------- wBus ---------------------------------------------
+
+      wBus(int I2C_Device_ID, int Max_Queue_Length, bool Log_To_Serial, long Serial_Speed);
+      void Boot_Message();
+      int Device_ID_Check();
+      void I2C_BUS_Error(int Error_Number);
+      void Broadcast(String Broadcast_String);
+
+      // --------------------------------------------- I2C Command Queue ---------------------------------------------
+
+      void Queue_Push(String Push_String, bool Add_To_Front_Of_Queue);
+      String Queue_Pop();
+      String Queue_Peek();
+      String Queue_Peek_Queue();
+      int Queue_Length();
+      int Queue_Is_Empthy();
+      void Queue_Clear();
+      String Queue_Search_Peek(String Search_String);
+      String Queue_Search_Pop(String Search_String, bool Delete_All_Matches);
+
+
+      // --------------------------------------------- Misc ---------------------------------------------
+
+      void Error_Blink(int Number_Of_Blinks);
 
 
 
 
-private:
-  // --------------------------------------------- General ---------------------------------------------
+    private:
+      // --------------------------------------------- From MBWire ---------------------------------------------
 
-  int _Device_ID;
+      static uint8_t rxBuffer[];
+      static uint8_t rxBufferIndex;
+      static uint8_t rxBufferLength;
 
-  int _Log_To_Serial;
-  long _Serial_Speed;
+      static uint8_t txAddress;
+      static uint8_t txBuffer[];
+      static uint8_t txBufferIndex;
+      static uint8_t txBufferLength;
+
+      static uint8_t transmitting;
+      static void (*user_onRequest)(void);
+      static void (*user_onReceive)(int);
+      static void onRequestService(void);
+      static void onReceiveService(uint8_t*, int);
+
+      // --------------------------------------------- General ---------------------------------------------
+
+      int _Device_ID;
+
+      int _Log_To_Serial;
+      long _Serial_Speed;
 
 
-  // --------------------------------------------- Device ID Checkd ---------------------------------------------
+      // --------------------------------------------- Device ID Checkd ---------------------------------------------
 
-  int _Device_ID_Check_OK = 0;
+      int _Device_ID_Check_OK = 0;
 
 
-  // --------------------------------------------- I2C Command Queue ---------------------------------------------
+      // --------------------------------------------- I2C Command Queue ---------------------------------------------
 
-  int _Max_Queue_Length;
-  int _Queue_Length = 0;
-  bool _Queue_Is_Empthy = true;
-  String _Queue_String = ";"; // ";" is used as seperators betweens the string in the queue
+      int _Max_Queue_Length;
+      int _Queue_Length = 0;
+      bool _Queue_Is_Empthy = true;
+      String _Queue_String = ";"; // ";" is used as seperators betweens the string in the queue
 
-};
+  };
 
 #endif
