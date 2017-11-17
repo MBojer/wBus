924113e5fb5367b8979b21e9c267414a6b6c87b0
Morten Bojer <morten.bojer@gmail.com>
2017-11-17 07:55:40 +0000

    Coped main.cpp to Mega1/main.cpp

--- Mega2/main.cpp
+++ Mega2/main.cpp
@@ -8,11 +8,14 @@ unsigned int Counter;
 
 String I2C_Receive_Data;
 
-int I2C_Address = 12;
+int I2C_Address = 11;
 
 int I2C_BUS_Responce;
 
-WBus wBus(I2C_Address, false, 20, true, 115200);
+
+
+
+WBus wBus(I2C_Address, true, 20, true, 115200, Loop_Delay);
 
 
 void I2C_Receive(int HowMany) {
@@ -21,39 +24,46 @@ void I2C_Receive(int HowMany) {
     I2C_Receive_Data += (char)wBus.read();
   }
 
+wBus.Queue_Push(I2C_Receive_Data, false);
+
 } //End marker for I2C_Receive
 
 
 void setup() {
-	Serial.begin(115200);
-	Serial.println("Boot Start");
+  Serial.begin(115200);
+  Serial.println("Boot Start");
 
-	wBus.Boot_Message();
+  randomSeed(analogRead(0)); // CHANGE ME
+  unsigned int Boot_Delay = random(0, 5000); // CHANGE ME
+  Serial.println("Random start delay: " + String(Boot_Delay) + " milliseconds"); // CHANGE ME
+  delay(Boot_Delay);
+  delay(5000);
 
-	// wBus.Device_ID_Check();
+  wBus.Boot_Message();
 
+  TWAR = (I2C_Address << 1) | 1;
+  wBus.onReceive(I2C_Receive);
 
-	TWAR = (I2C_Address << 1) | 1;
-	wBus.onReceive(I2C_Receive);
-
+  wBus.Device_ID_Check();
 
   Serial.println("Boot Done");
-	wBus.broadcast("Mega2 Boot Done");
-	delay(1000);
-	}
+  delay(1000);
+}
 
 void loop() {
   Counter++;
   // Serial.println("Counter: " + String(Counter));
 
+  wBus.Device_ID_Check();
+
   if (I2C_Receive_Data != "") {
     Serial.println("Reviced: " + I2C_Receive_Data);
-		I2C_Receive_Data = "";
+    I2C_Receive_Data = "";
   }
 
-	if (Counter % 7 == 0) {
-		wBus.broadcast("Mega2: " + String(Counter));
-	}
+  if (Counter % 7 == 0) {
+    wBus.broadcast("Mega2: " + String(Counter));
+  }
 
 
 
