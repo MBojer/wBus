9bb1289faca28c84d2e5761b48a7363bffbf5d0f
Morten Bojer <morten.bojer@gmail.com>
2017-11-20 16:00:07 +0000

    working it seems

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,22 @@
 
 #include <MemoryFree.h> // REMOVE ME
 
-String I2C_Receive_Data;
-
 int I2C_Address = 11;
 
 WBus wBus(I2C_Address, true, 20, true, 115200);
 
 void I2C_Receive(int HowMany) {
-  // String I2C_Receive_Data; // UNCOMMENT ME LATER - Used to make sure that the I2C_Receive_Data gets cleared every time
+  String I2C_Receive_Data; // UNCOMMENT ME LATER - Used to make sure that the I2C_Receive_Data gets cleared every time
 
   while (wBus.available()) {
     I2C_Receive_Data += (char)wBus.read();
   }
 
   wBus.Queue_Push(I2C_Receive_Data, false);
+
+  Serial.print("Resived: ");
+  Serial.println(I2C_Receive_Data);
+  I2C_Receive_Data = "";
 
 } //End marker for I2C_Receive
 
@@ -27,12 +29,11 @@ void Error_Mode(void) {
     wBus.Blink_LED_Start(wBus.I2C_BUS_Error());
   }
 
-  if (String(millis()).indexOf("000") <= 0) {
+  if (String(millis()).indexOf("000") >= 0) {
     Serial.println("ERROR MODE");
   }
 
 } // END MARKER - Error Mode
-
 
 
 void setup() {
@@ -41,35 +42,25 @@ void setup() {
   Serial.print("freeMemory()=");
   Serial.println(freeMemory());
 
-
   wBus.begin(I2C_Address);
-
-
 
   TWAR = (I2C_Address << 1) | 1;
   wBus.onReceive(I2C_Receive);
-
-
 
   Serial.print("freeMemory()=");
   Serial.println(freeMemory());
   Serial.println("Boot Done");
 }
 
+
 void loop() {
   wBus.Device_ID_Check();
-  wBus.Blink_LED(false);
   if (wBus.I2C_BUS_Error() != 0) {
     Error_Mode();
   }
+  wBus.Blink_LED(false);
 
 
-
-  if (I2C_Receive_Data != "") {
-    Serial.print("Resived: ");
-    Serial.println(I2C_Receive_Data);
-    I2C_Receive_Data = "";
-  }
 
   if (String(millis()).indexOf("000") >= 0) {
     Serial.print("freeMemory()=");
